c17ef460ef91ce119d0720b19cdb314977b9b567
frfrankie <frfrankie5@gmail.com>
2024-12-02 19:54:54 +0100

    footer component

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 =======
 >>>>>>> dd05d4e (feat: basic Angular layout)
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
 
