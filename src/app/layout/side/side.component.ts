0352b670be449b075d416fe33e1f4a23a6e5ac1e
keruch <verendeev.kirill.2000@gmail.com>
2024-12-02 20:36:20 +0100

    added login-register page

--- src/app/layout/side/side.component.ts
+++ src/app/layout/side/side.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-side',
@@ -10,6 +11,28 @@ import { Component } from '@angular/core';
   templateUrl: './side.component.html',
   styleUrls: ['./side.component.css']
 })
-export class SideComponent {
+export class SideComponent implements OnInit {
+  public username$: string;
+  public email$: string;
 
+  constructor (private authService : AuthService) {
+    this.username$ = ""
+    this.email$ = ""
+  }
+
+  ngOnInit(): void {
+    this.authService.getUserData().subscribe({
+      next: data => {
+        const d = data.data?.name
+        if (d != undefined) {
+          this.username$ = d
+        }
+
+        const e = data.data?.email
+        if (e != undefined) {
+          this.email$ = e
+        }
+      }
+    });
+  }
 }
--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -9,6 +9,7 @@ export class ArticlesService {
   constructor() { }
 =======
 import { HttpClient } from '@angular/common/http';
+import {AuthService} from "./auth.service";
 
 @Injectable({
   providedIn: 'root',
@@ -16,22 +17,30 @@ import { HttpClient } from '@angular/common/http';
 export class ArticlesService {
   private baseUrl = 'http://127.0.0.1:8000/api';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   getArticle(id: number) {
-    return this.http.get(`${this.baseUrl}/posts/${id}`);
+    return this.http.get(`${this.baseUrl}/posts/${id}`, {
+      headers: this.authService.createAuthHeaders()
+    });
   }
 
   getArticles() {
-    return this.http.get(`${this.baseUrl}/posts`);
+    return this.http.get(`${this.baseUrl}/posts`, {
+      headers: this.authService.createAuthHeaders()
+    });
   }
 
   deleteArticle(id: number) {
-    return this.http.delete(`${this.baseUrl}/posts/${id}`);
+    return this.http.delete(`${this.baseUrl}/posts/${id}`, {
+      headers: this.authService.createAuthHeaders()
+    });
   }
 
   createArticle(article: any) {
-    return this.http.post(`${this.baseUrl}/posts`, article);
+    return this.http.post(`${this.baseUrl}/posts`, article, {
+      headers: this.authService.createAuthHeaders()
+    });
   }
 >>>>>>> dd05d4e (feat: basic Angular layout)
 }
