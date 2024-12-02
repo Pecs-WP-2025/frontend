2942d9cd814e7c1f13deb2c06341060aeb825965
frfrankie <frfrankie5@gmail.com>
2024-12-02 19:55:04 +0100

    header component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { RegisterComponent } from '../register/register.component';
+import { LoginComponent } from '../login/login.component';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +11,16 @@ import { Component } from '@angular/core';
 =======
 >>>>>>> dd05d4e (feat: basic Angular layout)
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+    constructor(public dialog: MatDialog) {}
 
+    openLoginDialog(): void {
+        this.dialog.open(LoginComponent);
+    }
+
+    openRegisterDialog(): void {
+        this.dialog.open(RegisterComponent);
+    }
 }
