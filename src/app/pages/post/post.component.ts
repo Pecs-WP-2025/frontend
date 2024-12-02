5a06b3cdac43f8b7c2750aad3bf13cf6c7b62f18
frfrankie <frfrankie5@gmail.com>
2024-12-02 12:41:04 +0100

    post component

--- src/app/pages/post/post.component.ts
+++ src/app/pages/post/post.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticlesService } from '../../services/articles.service';
 
 @Component({
+<<<<<<< HEAD
   selector: 'app-post',
 <<<<<<< HEAD
   standalone: true,
@@ -9,7 +12,28 @@ import { Component } from '@angular/core';
 >>>>>>> dd05d4e (feat: basic Angular layout)
   templateUrl: './post.component.html',
   styleUrl: './post.component.css'
+=======
+    selector: 'app-single-post',
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.css'],
+>>>>>>> 9876383 (feat: added post component)
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+    post: any;
+    id: number = 0;
+    constructor(
+        private articleService: ArticlesService,
+        private route: ActivatedRoute
+    ) {}
 
+    ngOnInit(): void {
+        // Gettig article id from the url
+        this.route.params.subscribe((param) => {
+            this.id = parseInt(param['id']);
+        });
+
+        this.articleService.getArticle(this.id).subscribe((data) => {
+            this.post = data;
+        });
+    }
 }
