ce8dfadbd139279877686582845fd653ec30d302
frfrankie <frfrankie5@gmail.com>
2024-12-02 19:35:58 +0100

    recent component

--- src/app/layout/recent/recent.component.ts
+++ src/app/layout/recent/recent.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+<<<<<<< HEAD
   selector: 'app-recent',
 <<<<<<< HEAD
   standalone: true,
@@ -8,10 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './recent.component.html',
   styleUrl: './recent.component.css'
 =======
+=======
+  selector: 'app-recent-section',
+>>>>>>> 4c43ae5 (feat: added recent component)
   templateUrl: './recent.component.html',
   styleUrls: ['./recent.component.css']
 >>>>>>> dd05d4e (feat: basic Angular layout)
 })
 export class RecentComponent {
-
+    
 }
